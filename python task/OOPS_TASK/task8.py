77b3d8062e98892471ee97f70641dec4dbd63b3c
Dhanish Kumar V <dhanishkumar.v>
2024-08-07 15:33:35 +0530

    uploading task

---
+++ python task/OOPS_TASK/task8.py
@@ -0,0 +1,39 @@
+class shoppingCart:
+    available_items={"Chicken":260,"Mutton":900,"Bread":50}
+    def __init__(self):
+        self.items={}
+    def addItems(self):
+        item=input("Enter item name: ")
+        if item in shoppingCart.available_items:
+            self.items[item]=shoppingCart.available_items[item]
+            print("Item added successfully")
+        else:
+            print("item not in the list")
+    def removeItems(self):
+        item=input("Enter item remove: ")
+        if item in self.items: 
+            del self.items[item]
+            print("Item removed successfully")
+        else:
+            print("Item not in the list")
+    def totalItems(self):
+        for item,price in self.items.items():
+            print(item)
+    def totalPrice(self):
+        totalPrice=0
+        for item,price in self.items.items():
+            totalPrice+=price
+        print(totalPrice)
+s=shoppingCart()
+while(True):
+    choice=int(input("Enter your choice"))
+    if choice==1:
+        s.addItems()
+    elif choice==2:
+        s.removeItems()
+    elif choice==3:
+        s.totalItems()
+    elif choice==4:
+        s.totalPrice()
+    else:
+        break
---
+++ python task/OOPS_TASK/task9.py
@@ -0,0 +1,19 @@
+def countstringfrequency(filename, searchstring):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+    except FileNotFoundError:
+        print(f"The file {filename} does not exist")
+        return
+
+    contentlower = content.lower()
+    searchstringlower = searchstring.lower()
+
+    frequency = contentlower.count(searchstringlower)
+
+    print(f"The string '{searchstring}' appears {frequency} times")
+
+filename = input("Enter the file name: ")
+searchstring = input("Enter the string to search for: ")
+
+countstringfrequency(filename, searchstring)
